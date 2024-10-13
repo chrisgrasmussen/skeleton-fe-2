c2371a12e909258864ee3d09126e24671428d2a3
chrisgrasmussen <chrisrasmussenphotography@gmail.com>
2024-10-12 17:25:04 -0700

    added navbar functionality

--- skeleton-fe-2/src/components/app/NavBar.jsx
+++ skeleton-fe-2/src/components/app/NavBar.jsx
@@ -4,11 +4,16 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div className="flex flex-row justify-between items-center">
-            <Link to={'/start'}>
-                <button className="bg-yellow-200 hover:bg-yellow-300 px-4 py-1">Conversations</button>
-            </Link>
-            <button className="bg-indigo-400 hover:bg-indigo-500 px-4 py-1 text-white">Login</button>
+            <div className="flex flex-row items-start">
+                <Link to={'/'}>
+                    <button className="bg-indigo-600 hover:bg-indigo-700 px-4 py-1 text-white">Home</button>
+                </Link>
+                <Link to={'/start'}>
+                    <button className="bg-yellow-200 hover:bg-yellow-300 px-4 py-1">Conversations</button>
+                </Link>
 
+            </div>
+            <button className="bg-indigo-400 hover:bg-indigo-500 px-4 py-1 text-white">Login</button>
         </div>
     )
 }
